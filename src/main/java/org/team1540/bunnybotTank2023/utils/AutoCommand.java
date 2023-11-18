4decc56f195103373afe477fe0f94ad84ff999df
Alvin Zhang <41vin2h4n9@gmail.com>
2023-11-17 20:10:39 -0800

    feat: loading path groups and a generic testing auto

--- src/main/java/org/team1540/bunnybotTank2023/utils/AutoCommand.java
+++ src/main/java/org/team1540/bunnybotTank2023/utils/AutoCommand.java
@@ -8,13 +8,28 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.team1540.bunnybotTank2023.commands.drivetrain.Drivetrain;
 import org.team1540.bunnybotTank2023.commands.drivetrain.PathPlannerDriveCommand;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class AutoCommand extends SequentialCommandGroup {
 
     public Command getPathPlannerDriveCommand(Drivetrain drivetrain, String pathName, PathConstraints constraints, boolean resetToPath) {
         PathPlannerTrajectory trajectory = PathPlanner.loadPath(pathName, constraints);
-        //TODO: Figure out if this is right
-        return new PathPlannerDriveCommand(drivetrain, trajectory, resetToPath);
+        return new PathPlannerDriveCommand(drivetrain, trajectory, resetToPath).asProxy();
     }
 
-
+    public List<Command> getPathPlannerDriveCommandGroup(
+            Drivetrain drivetrain,
+            String pathName,
+            PathConstraints[] constraints,
+            boolean resetToPath) {
+        List<PathPlannerTrajectory> trajectories = PathPlanner.loadPathGroup(pathName, Arrays.asList(constraints));
+        LinkedList<Command> commands = new LinkedList<>();
+        commands.addLast(new PathPlannerDriveCommand(drivetrain, trajectories.get(0), resetToPath).asProxy());
+        for (int i = 1; i < trajectories.size(); i++) {
+            commands.addLast(new PathPlannerDriveCommand(drivetrain, trajectories.get(0), false).asProxy());
+        }
+        return commands;
+    }
 }
