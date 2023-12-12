cb626b7acce4770a191c5e8a482c88c8047d14ab
PichuIsBest <maddoxrg@icloud.com>
2023-12-12 15:30:11 -0800

    added second solenoid

--- src/main/java/org/team1540/bunnybotTank2023/io/intake/IntakeIOReal.java
+++ src/main/java/org/team1540/bunnybotTank2023/io/intake/IntakeIOReal.java
@@ -11,21 +11,24 @@ import org.team1540.bunnybotTank2023.Constants;
 import javax.naming.ldap.Control;
 
 public class IntakeIOReal implements IntakeIO {
-    private final Solenoid solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.IntakeConstants.SOLENOID_PORT_NUMBER);
+    private final Solenoid solenoid1 = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.IntakeConstants.SOLENOID_1_PORT_NUMBER);
+    private final Solenoid solenoid2 = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.IntakeConstants.SOLENOID_2_PORT_NUMBER);
     private final VictorSPX motor = new VictorSPX(Constants.IntakeConstants.MOTOR_ID);
     public IntakeIOReal() {
-        solenoid.set(false);
+        solenoid1.set(false);
+        solenoid2.set(false);
         motor.setNeutralMode(NeutralMode.Brake);
         motor.setInverted(false);
     }
     @Override
     public void setFold(boolean fold) {
-        solenoid.set(fold);
+        solenoid1.set(fold);
+        solenoid2.set(fold);
     }
 
     @Override
     public void updateInputs(IntakeInputs inputs) {
-        inputs.folded = solenoid.get();
+        inputs.folded = solenoid1.get();
         inputs.motorAppliedVolts = motor.getMotorOutputVoltage();
         inputs.motorCurrentAmps = motor.getMotorOutputPercent();
         inputs.motorVelocity = motor.getActiveTrajectoryVelocity();
